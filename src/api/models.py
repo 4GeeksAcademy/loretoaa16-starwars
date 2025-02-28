585b28dacca6de6ed8f20bbb102bf52bd47abce4
Loreto Anguita Alonso <loreto16@hotmail.es>
2025-02-28 17:38:53 +0000

    class 1

--- src/api/models.py
+++ src/api/models.py
@@ -1,4 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 
 db = SQLAlchemy()
@@ -9,13 +10,40 @@ class Users(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    first_name = db.Column(db.String, nullable=True)
+    last_name = db.Column(db.String)
 
     def __repr__(self):
         return f'<User {self.email}>'
 
     def serialize(self):
           # do not serialize the password, its a security breach
-        return {"id": self.id,
-                "email": self.email,
-                "is_active": self.is_active}
+        return {'id': self.id,
+                'email': self.email,
+                'is_active': self.is_active,
+                'first_name': self.first_name,
+                'last_name': self.last_name}
     
+
+class Products(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=True, nullable=False)
+    description = db.Column(db.String, unique=False, nullable=True)
+    price = db.Column(db.Float, nullable=False)
+
+class Bills(db.Model):
+    __tablename__='bills'
+    id = db.Column(db.Integer, primary_key=True)
+    create_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow) #default, creation day
+    total = db.Column(db.Float, nullable=False)
+    bill_address = db.Column(db.String)
+    status = db.Column(db.Enum('pending', 'paid', 'cancel', name='status'), nullable=False)
+    payment = db.Column(db.Enum('visa', 'amex', 'paypal', name='payment'), nullable=False)
+
+
+class BillItems(db.Model):
+    __tablename__='bills_items'
+    id = db.Column(db.Integer, primary_key=True)
+    price_per_unit = db.Column(db.Float, nullable=False)
+    quantity = db.Column(db.Integer, nullable=False)
+    total_price = db.Column(db.Float, nullable=False)
